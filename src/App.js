34e051dcebfb53f8f3fc6e2fc7c3846daf62c43f
MHRumon <rumonuniv@gmail.com>
2022-01-10 00:52:14 +0900

    canvas-basics

--- src/App.js
+++ src/App.js
@@ -1,39 +1,66 @@
 import React,{useState, useRef, useEffect} from 'react';
-import { Box } from '@material-ui/core';
-import { useAnimationHook } from './utils/useAnimationHook'; // central animation hook
-import {PlayerBlock} from './utils/PlayerBlock';
-import { styleObjects } from './utils/styleObjects';
+import { useInterval } from './utils/useInterval';
+import { Button } from '@material-ui/core';
+// constants
+import { CANVAS_SIZE,
+APPLE_START,
+SNAKE_START,
+SCALE,
+SPEED,
+DIRECTIONS } from './utils/constants';
 
 const App = () => {
-    const classes = styleObjects();
-    const gameBoardRef= useRef(null);
-    // playerBlock functions
-    const {DrawPlayerBlock} = PlayerBlock(gameBoardRef);
+
+    const canvasRef = useRef(null);
+    const [snake, setSnake] = useState(SNAKE_START);
+    const [apple, setApple] = useState(APPLE_START);
+    const [direction, setDirections] = useState([0, -1]);
+    const [speed, setSpeed] = useState(null);
+    const [gameLose, setGameLose] = useState(false);
+
+    const startGame = ()=>{
+
+    }
+
+    const gameOver = () =>{
+
+    }
+
+    const moveSnake = ()=>{
+
+    }
+
+    const createApple = ()=>{
+
+    }
+
+    const checkCollision = ()=>{
+
+    }
+
+    const checkAppleEat = ()=>{
+
+    }
+
+    const gameLoop = () =>{
+
+    }
 
     useEffect(()=>{
-        const gameBoardCurrent = gameBoardRef.current;
-        console.log(gameBoardCurrent)
-    },[])
+        const context = canvasRef.current.getContext("2d");
+    },[snake, apple, gameLose])
 
-    // main update and draw loop
-    useAnimationHook((deltaTime)=>{
-        updateGame();
-        drawGame();
-    })
-
-    const updateGame = ()=>{
-
-    }
-    const drawGame = ()=>{
-        DrawPlayerBlock(gameBoardRef);
-    }
-
- 
     return (
-        <div className={classes.root}>
-            <Box className={classes.gameBoard} ref={gameBoardRef}>
-                
-            </Box>
+        <div role={'button'} style={{display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center'}} tabIndex={0} onKeyDown={e=> moveSnake(e)}>
+            <canvas style={{border: "2px solid black"}}
+                ref={canvasRef}
+                width={`${CANVAS_SIZE[0]}px`}
+                height={`${CANVAS_SIZE[1]}px`}/>
+            {gameLose && <div>GAME OVER!</div>}
+            <Button onClick={startGame} variant='contained'>Start Game</Button>
         </div>
     )
 };
